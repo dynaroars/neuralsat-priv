1794fead6d87c557909a851c449f7f0b02cd84fc
Hai <20141340@student.hust.edu.vn>
2024-07-13 18:33:28 -0400

    update vnncomp scripts

--- vnncomp_scripts/prepare_instance.sh
+++ vnncomp_scripts/prepare_instance.sh
@@ -1,13 +1,18 @@
-if [[ -z "${DNNV_PYTHON}" ]]; then
-	DNNV_PYTHON=${HOME}/anaconda3/envs/dnnv/bin/python3
-fi
+CONDA_HOME=~/conda
+CONDA=$CONDA_HOME/bin/conda
+
+DNNV_CONDA_HOME=~/.conda/envs/dnnv
+DNNV_PY=$DNNV_CONDA_HOME/bin/python
 
 # echo $DNNV_PYTHON
-$DNNV_PYTHON -c "import dnnv; print('DNNV version', dnnv.__version__)"
-$DNNV_PYTHON -c "import onnxsim; print('ONNXSIM version', onnxsim.__version__)"
+$DNNV_PY -c "import dnnv; print('DNNV version', dnnv.__version__)"
+$DNNV_PY -c "import onnxsim; print('ONNXSIM version', onnxsim.__version__)"
 
-CATEGORY=$1
-ONNX_FILE=$2
+CATEGORY=$2
+ONNX_FILE=$3
+VNNLIB_FILE=$4
+RESULTS_FILE=$5
+TIMEOUT=$6
 
 echo "Simplifying for benchmark '$CATEGORY' with onnx file '$ONNX_FILE'"
 
@@ -21,7 +26,8 @@ if [ -d $OUTPUT_DIR ]; then
 	rm -r $OUTPUT_DIR
 fi
 
-if [ "${CATEGORY,,}" == "vggnet16" ] || [ "${CATEGORY,,}" == "cgan" ]; then
-    $DNNV_PYTHON $TOOL_DIR/neuralsat-pt201/util/network/simplify_onnx.py $ONNX_FILE $OUTPUT_DIR/model-simplified
-fi
+$DNNV_PY $TOOL_DIR/neuralsat-pt201/util/network/simplify_onnx.py $ONNX_FILE $OUTPUT_DIR/model-simplified
+# if [ "${CATEGORY,,}" == "vggnet16" ] || [ "${CATEGORY,,}" == "cgan" ]; then
+#     $DNNV_PY $TOOL_DIR/neuralsat-pt201/util/network/simplify_onnx.py $ONNX_FILE $OUTPUT_DIR/model-simplified
+# fi
 exit 0
