d2fbfdbf892d373a9fdfa6a0b700144693e8efb0
thanh <lethanh@nii.ac.jp>
2024-11-17 04:51:00 +0900

    x

--- test.py
+++ test.py
@@ -4,43 +4,45 @@ from neuralsat import InteractiveVerifier
 # import logging
 
 if __name__ == "__main__":
-    
+
     ############## Preprocess ##############
-    
+
     # logger.setLevel(logging.INFO)
     reset_settings()
-    
+
     net_path = 'src/neuralsat/example/onnx/mnistfc-medium-net-151.onnx'
     vnnlib_path = 'src/neuralsat/example/vnnlib/prop_2_0.03.vnnlib'
     device = 'cpu'
     batch = 100
     print(f'\n\nRunning test with {net_path=} {vnnlib_path=}')
-    
+
     model, input_shape, objectives = extract_instance(net_path, vnnlib_path)
     model.to(device)
-    
+
     env = InteractiveVerifier(
         net=model,
         input_shape=input_shape,
         batch=batch,
         device=device,
     )
-    
+
     objectives, _ = env._preprocess(objectives, force_split=None)
     objective = objectives.pop(1)
-    
+
     ############## Verify ##############
-    
+
 
     done = env.init(
-        objective=objective, 
+        objective=objective,
         reference_bounds=None,
-        preconditions={}, 
+        preconditions={},
     )
-    
+
     while not done:
-        obs, subproblems = env.get_observation(batch)
-        action, _state = env.decide(obs, subproblems)
+        observation, subproblems = env.get_observation(batch)
+        action, _state = env.decide(observation, subproblems)
         reward, done, info = env.step(action)
-        print(info)
-    
+        for i in range(len(observation)):
+            for j in range(len(observation[i])):
+                print(f'observation[{i=}][{j=}]={observation[i][j].shape}')
+        exit()
